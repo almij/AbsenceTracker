852b92fe1f353a4511c1a06f7af04186f5bfaee8
Alexander.Mikhaylov <Alexander.Mikhaylov@totalobjects.spb.ru>
2019-09-09 17:45:19 +0300

    PersonalData form

---
+++ source/AbsenceTracker/AbsenceTrackerUI/Forms/AbsenceTrackerDashboard.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AbsenceTrackerUI.Forms
+{
+    public partial class AbsenceTrackerDashboard : Form
+    {
+        public AbsenceTrackerDashboard()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ source/AbsenceTracker/AbsenceTrackerUI/Forms/PersonalData.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AbsenceTrackerUI.Forms
+{
+    public partial class PersonalData : Form
+    {
+        public PersonalData()
+        {
+            InitializeComponent();
+        }
+
+        private void PersonalData_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ProjectsInvolvedInLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
