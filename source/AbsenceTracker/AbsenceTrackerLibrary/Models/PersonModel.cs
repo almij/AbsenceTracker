e357d3e1487bcbb34692e2db09db17a36ddf0a79
Alexander.Mikhaylov <Alexander.Mikhaylov@totalobjects.spb.ru>
2019-09-09 11:17:47 +0300

    re-created proj with gitignore in place

---
+++ source/AbsenceTracker/AbsenceTrackerLibrary/Models/PersonModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace AbsenceTrackerLibrary.Models
+{
+    public class PersonModel
+    {
+        public int PersonId { get; set; }
+        public string Username { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Patronymic { get; set; }
+        public string MiddleName { get; set; }
+        public string Email { get; set; }
+        public string FullNameForDocuments { get; set; }
+        public DateTime StartedAt { get; set; }
+        public ProjectModel Project { get; set; }
+        public DepartmentModel Department { get; set; }
+        public int DaysOffBalance { get; set; }
+    }
+}
