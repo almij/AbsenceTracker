75b3fe7a73a6e5c352f77be7b6cf099a30b5c1fc
Alexander.Mikhaylov <Alexander.Mikhaylov@totalobjects.spb.ru>
2019-09-13 13:30:45 +0300

    worksave

--- source/AbsenceTracker/AbsenceTrackerUI/Forms/AbsenceDetails.cs
+++ source/AbsenceTracker/AbsenceTrackerUI/Forms/AbsenceDetails.cs
@@ -1,13 +1,7 @@
 ﻿using AbsenceTrackerLibrary;
 using AbsenceTrackerLibrary.Models;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace AbsenceTrackerUI.Forms
