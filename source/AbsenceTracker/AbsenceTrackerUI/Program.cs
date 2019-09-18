46cb8c0f385355f722a98d10428c1dde73566561
Alexander.Mikhaylov <Alexander.Mikhaylov@totalobjects.spb.ru>
2019-09-18 18:23:37 +0300

    work save

--- source/AbsenceTracker/AbsenceTrackerUI/Program.cs
+++ source/AbsenceTracker/AbsenceTrackerUI/Program.cs
@@ -9,13 +9,16 @@ namespace AbsenceTrackerUI
     {
         /// <summary>
         /// The main entry point for the application.
+        /// TODO look more into why in UI proj you had to add references at nugets that are already added as dependencies into referenced library
+        /// this looks related: Issues with .NET Standard 2.0 with .NET Framework & NuGet #481
+        /// https://github.com/dotnet/standard/issues/481 
         /// </summary>
         [STAThread]
         static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Config.Initialise(Properties.Settings.Default.DatabaseConnection, "1");
+            Config.Initialise(Properties.Settings.Default.DatabaseConnection, Properties.Settings.Default.DefaultUserId);
             Application.Run(new AbsenceTrackerDashboard());
         }
     }
