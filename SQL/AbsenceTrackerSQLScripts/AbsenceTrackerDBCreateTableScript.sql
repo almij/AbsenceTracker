89c9560fac6231b02cef8b6a2e01a817604de9d2
Alexander.Mikhaylov <Alexander.Mikhaylov@totalobjects.spb.ru>
2019-09-19 11:35:52 +0300

    moved fk constraints after creating tables
    now script can actually be executed all at once

--- SQL/AbsenceTrackerSQLScripts/AbsenceTrackerDBCreateTableScript.sql
+++ SQL/AbsenceTrackerSQLScripts/AbsenceTrackerDBCreateTableScript.sql
@@ -1,3 +1,5 @@
+--USE master;
+--DROP DATABASE AbsenceTracker;
 --CREATE DATABASE AbsenceTracker;
 
 USE AbsenceTracker;
@@ -6,23 +8,21 @@ CREATE TABLE dbo.department
 (
     department_id INT NOT NULL IDENTITY,
     department_name NVARCHAR(100) NOT NULL,
-    department_head_id INT, 
-
-    CONSTRAINT pk_department_id PRIMARY KEY (department_id),
-    CONSTRAINT fk_department_person FOREIGN KEY (department_head_id) REFERENCES Person(person_id)
+    department_head_id INT,
+		
+    CONSTRAINT pk_department_id PRIMARY KEY (department_id)
 );
-GO;
+GO
 
 CREATE TABLE dbo.project
 (
     project_id int NOT NULL IDENTITY,
     project_name nvarchar(100) NOT NULL,
-    project_head_id INT, 
-
+    project_head_id INT,
+	
     CONSTRAINT pk_project_id PRIMARY KEY (project_id),
-    CONSTRAINT fk_project_person FOREIGN KEY (project_head_id) REFERENCES Person(person_id)
 );
-GO;
+GO
 
 CREATE TABLE dbo.person
 (
@@ -38,54 +38,47 @@ CREATE TABLE dbo.person
     started_at DATE,
     department_id INT NULL,
     days_off_balance INT,
-
+		
     CONSTRAINT unique_person_username UNIQUE (username),
     CONSTRAINT pk_person_id PRIMARY KEY (person_id),
-    CONSTRAINT fk_person_department FOREIGN KEY(department_id) REFERENCES department(department_id)
 );
-GO;
+GO
 
 CREATE TABLE dbo.personnel_substitution
 (
     person_id INT NOT NULL,
     substitute_person_id INT NOT NULL,
-
-    CONSTRAINT fk_substitute_person_1 FOREIGN KEY (person_id) REFERENCES Person(person_id),
-    CONSTRAINT fk_substitute_person_2 FOREIGN KEY (substitute_person_id) REFERENCES Person(person_id),
+	
     CONSTRAINT pk_substitute_combo PRIMARY KEY (person_id, substitute_person_id)
 );
-GO;
+GO
 
 CREATE TABLE dbo.project_personnel
 (
     project_id INT NOT NULL,
     person_id INT NOT NULL,
 
-    CONSTRAINT fk_project_team_project FOREIGN KEY (project_id) REFERENCES project(project_id),
-    CONSTRAINT fk_project_team_person FOREIGN KEY (person_id) REFERENCES person(person_id),
     CONSTRAINT pk_project_team_combo PRIMARY KEY (project_id, person_id)
 );
-GO;
+GO
 
 CREATE TABLE dbo.supervision
 (
     person_id INT NOT NULL,
     supervisor_person_id INT NOT NULL,
 
-    CONSTRAINT fk_supervision_person_1 FOREIGN KEY (person_id) REFERENCES person(person_id),
-    CONSTRAINT fk_supervision_person_2 FOREIGN KEY (supervisor_person_id) REFERENCES person(person_id),
     CONSTRAINT pk_supervision_combo PRIMARY KEY (person_id, supervisor_person_id)
 );
-GO;
+GO
 
 CREATE TABLE dbo.absence_type
 (
     absence_type_id INT NOT NULL IDENTITY,
     absence_type_name NVARCHAR(100) NOT NULL,
-
+	
     CONSTRAINT pk_absence_type_id PRIMARY KEY (absence_type_id)
 );
-GO;
+GO
 
 CREATE TABLE dbo.absence
 (
@@ -94,15 +87,45 @@ CREATE TABLE dbo.absence
     person_id INT NOT NULL,
     effective_from DATE NOT NULL,
     expires_on DATE NOT NULL,
-    days_worked_on_holidays INT,
-    
+    days_worked_on_holidays INT CONSTRAINT default_absence_days_worked_on_holidays_zero DEFAULT 0,
+	
     CONSTRAINT pk_absence_id PRIMARY KEY (absence_id),
-    CONSTRAINT fk_absense_absence_type FOREIGN KEY (absence_type_id) REFERENCES absence_type(absence_type_id),
-    CONSTRAINT fk_absence_person FOREIGN KEY (person_id) REFERENCES person(person_id),
-    CONSTRAINT check_absence_effective_date_le_expiration_date 
-        CHECK(effective_from <= expires_on),
-    CONSTRAINT unique_absence_person_id_effective_date_expiration_date
-        UNIQUE(person_id, effective_from, expires_on),
-    CONSTRAINT default_absence_days_worked_on_holidays_zero DEFAULT 0 for days_worked_on_holidays
+    CONSTRAINT check_absence_effective_date_le_expiration_date CHECK(effective_from <= expires_on),
+    CONSTRAINT unique_absence_person_id_effective_date_expiration_date UNIQUE(person_id, effective_from, expires_on)
 );
-GO;
+GO
+
+
+
+
+ALTER TABLE dbo.department ADD
+    CONSTRAINT fk_department_person FOREIGN KEY (department_head_id) REFERENCES dbo.Person(person_id)
+GO
+
+ALTER TABLE dbo.project ADD
+    CONSTRAINT fk_project_person FOREIGN KEY (project_head_id) REFERENCES dbo.Person(person_id)
+GO
+
+ALTER TABLE dbo.person ADD
+    CONSTRAINT fk_person_department FOREIGN KEY(department_id) REFERENCES dbo.department(department_id)
+GO
+
+ALTER TABLE dbo.personnel_substitution ADD
+    CONSTRAINT fk_substitute_person_1 FOREIGN KEY (person_id) REFERENCES dbo.Person(person_id),
+    CONSTRAINT fk_substitute_person_2 FOREIGN KEY (substitute_person_id) REFERENCES dbo.Person(person_id)
+GO
+
+ALTER TABLE dbo.project_personnel ADD
+    CONSTRAINT fk_project_team_project FOREIGN KEY (project_id) REFERENCES dbo.project(project_id),
+    CONSTRAINT fk_project_team_person FOREIGN KEY (person_id) REFERENCES dbo.person(person_id)
+GO
+
+ALTER TABLE dbo.supervision ADD
+    CONSTRAINT fk_supervision_person_1 FOREIGN KEY (person_id) REFERENCES dbo.person(person_id),
+    CONSTRAINT fk_supervision_person_2 FOREIGN KEY (supervisor_person_id) REFERENCES dbo.person(person_id)
+GO
+
+ALTER TABLE dbo.absence ADD
+    CONSTRAINT fk_absense_absence_type FOREIGN KEY (absence_type_id) REFERENCES dbo.absence_type(absence_type_id),
+    CONSTRAINT fk_absence_person FOREIGN KEY (person_id) REFERENCES dbo.person(person_id)
+GO
